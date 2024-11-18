07c4e94088d40ac606ad176d5cbd2fbd6efd3439
LesegoKhaole <ST10455441@imconnect.edu.za>
2024-11-18 03:36:04 +0200

    log change

--- App.tsx
+++ App.tsx
@@ -40,7 +40,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f2b704',
+    backgroundColor: '#abf204',
     alignItems: 'center',
     justifyContent: 'center',
   },
